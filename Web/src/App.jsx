b8b8e1926de88158dca104f1e7f0bf69e084e9dc
mukuvi <jamesmngandu@gmail.com>
2025-05-12 09:28:41 +0300

    Ecosense

--- Web/src/App.jsx
+++ Web/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold text-center">Ecosense!</h1>
     </>
   );
 }
