2f7172ff10f8e8966fd03379915408ad97caaf73
mukuvi <jamesmngandu@gmail.com>
2025-05-20 11:47:01 +0300

    Ecosense

--- Web/src/App.jsx
+++ Web/src/App.jsx
@@ -1,10 +1,19 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Authenticate from "./Pages/Authenticate";
+import UserDashboard from "./Pages/UserDashboard";
+import AdminDashboard from "./Pages/AdminDashboard";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold text-center">Ecosense!</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Authenticate />} />
+
+        <Route path="/User" element={<UserDashboard />} />
+        <Route path="/Admin" element={<AdminDashboard />} />
+      </Routes>
+    </Router>
   );
 }
 
